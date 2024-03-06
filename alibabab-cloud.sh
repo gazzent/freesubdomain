e58130b03e1503939b570732bc43a77b14799647
gazzent <158091173+gazzent@users.noreply.github.com>
2024-03-06 17:34:23 +0700

    Create alibabab-cloud.sh

---
+++ alibabab-cloud.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+LIST AKUN BELUM TERJUAL
+_____________________
+1. LOGIN PANEL READY TO SELL
+EMAIL GMAIL.COM
+EMAIL : askadzikri64@gmail.com
+PASS  : 123Memek123@
+
+CLOUD : https://id.alibabacloud.com/id
+EMAIL : askadzikri64@gmail.com
+PASS  : 123Memek123@
+______________________
+2. LOGIN PANEL READY TO SELLING
+LOGIN EMAIL gmai.com
+EMAIL : techirome@gmail.com
+PASS  : 123Memek123@
+
+CLOUD : https://id.alibabacloud.com/id
+EMAIL : techirome@gmail.com
+PASS  : 123Memek123@
+_______________________
+BY CANDRAVPNZ
